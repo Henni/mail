0d0a85c0b4e458920286989047d1fa2642f9613c
Thomas Müller <thomas.mueller@tmit.eu>
2015-04-19 22:11:41 +0200

    Decrypt password of mail accounts

--- lib/service/autoconfig.php
+++ lib/service/autoconfig.php
@@ -15,6 +15,7 @@ namespace OCA\Mail\Service;
 use Exception;
 use OCA\Mail\Account;
 use OCA\Mail\Db\MailAccount;
+use OCP\Security\ICrypto;
 
 class AutoConfig {
 
@@ -30,12 +31,18 @@ class AutoConfig {
 	private $userId;
 
 	/**
+	 * @var ICrypto
+	 */
+	private $crypto;
+
+	/**
 	 * @param Logger $logger
 	 * @param string $userId
 	 */
-	public function __construct(Logger $logger, $userId) {
+	public function __construct(Logger $logger, $userId, ICrypto $crypto) {
 		$this->logger = $logger;
 		$this->userId = $userId;
+		$this->crypto = $crypto;
 	}
 
 	/**
@@ -105,6 +112,7 @@ class AutoConfig {
 							$account->setOutboundHost($url);
 							$account->setOutboundPort($port);
 							$account->setOutboundUser($user);
+							$password = $this->crypto->decrypt($password);
 							$account->setOutboundPassword($password);
 							$account->setOutboundSslMode($protocol);
 
@@ -182,6 +190,7 @@ class AutoConfig {
 
 						$account->setOutboundHost($smtp['hostname']);
 						$account->setOutboundPort($smtp['port']);
+						$password = $this->crypto->decrypt($password);
 						$account->setOutboundPassword($password);
 						$account->setOutboundUser($user);
 						$account->setOutboundSslMode(strtolower($smtp['socketType']));
@@ -373,6 +382,7 @@ class AutoConfig {
 		$account->setInboundPort($port);
 		$account->setInboundSslMode($encryptionProtocol);
 		$account->setInboundUser($user);
+		$password = $this->crypto->decrypt($password);
 		$account->setInboundPassword($password);
 
 		$a = new Account($account);
